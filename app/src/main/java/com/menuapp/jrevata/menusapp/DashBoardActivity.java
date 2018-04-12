9db5924dbf26a71884c0c4e3bf127330913ae187
Jrevata <jordan.revata@tecsup.edu.pe>
2018-04-12 10:58:27 -0500

    Fourth commit

--- app/src/main/java/com/menuapp/jrevata/menusapp/DashBoardActivity.java
+++ app/src/main/java/com/menuapp/jrevata/menusapp/DashBoardActivity.java
@@ -2,10 +2,18 @@ package com.menuapp.jrevata.menusapp;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.menuapp.jrevata.menusapp.Fragments.FifthFragment;
+import com.menuapp.jrevata.menusapp.Fragments.FirstFragment;
+import com.menuapp.jrevata.menusapp.Fragments.FourthFragment;
+import com.menuapp.jrevata.menusapp.Fragments.SecondFragment;
+import com.menuapp.jrevata.menusapp.Fragments.ThirdFragment;
 
 public class DashBoardActivity extends AppCompatActivity {
 
@@ -14,25 +22,42 @@ public class DashBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
+
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.menu_home:
                         Toast.makeText(DashBoardActivity.this, "Go home section...", Toast.LENGTH_SHORT).show();
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        Fragment fragment = new FirstFragment();
+                        fragmentManager.beginTransaction().replace(R.id.content, fragment).addToBackStack("tag").commit();
                         break;
                     case R.id.menu_camera:
                         Toast.makeText(DashBoardActivity.this, "Go camera section...", Toast.LENGTH_SHORT).show();
+                        fragmentManager = getSupportFragmentManager();
+                        fragment = new SecondFragment();
+                        fragmentManager.beginTransaction().replace(R.id.content, fragment).addToBackStack("tag").commit();
                         break;
                     case R.id.menu_search:
                         Toast.makeText(DashBoardActivity.this, "Go share section...", Toast.LENGTH_SHORT).show();
+                        fragmentManager = getSupportFragmentManager();
+                        fragment = new ThirdFragment();
+                        fragmentManager.beginTransaction().replace(R.id.content, fragment).addToBackStack("tag").commit();
                         break;
                     case R.id.menu_notification:
                         Toast.makeText(DashBoardActivity.this, "Go notification section...", Toast.LENGTH_SHORT).show();
+                        fragmentManager = getSupportFragmentManager();
+                        fragment = new FourthFragment();
+                        fragmentManager.beginTransaction().replace(R.id.content, fragment).addToBackStack("tag").commit();
                         break;
                     case R.id.menu_more:
                         Toast.makeText(DashBoardActivity.this, "Go more sections...", Toast.LENGTH_SHORT).show();
+                        fragmentManager = getSupportFragmentManager();
+                        fragment = new FifthFragment();
+                        fragmentManager.beginTransaction().replace(R.id.content, fragment).addToBackStack("tag").commit();
                         break;
                 }
                 return true;
---
+++ app/src/main/java/com/menuapp/jrevata/menusapp/Fragments/FourthFragment.java
@@ -0,0 +1,49 @@
+package com.menuapp.jrevata.menusapp.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.menuapp.jrevata.menusapp.R;
+
+/**
+ * Created by Alumno on 12/04/2018.
+ */
+
+public class FourthFragment extends Fragment {
+
+    private static final String TAG = FirstFragment.class.getSimpleName();
+
+    public FourthFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+        View view = inflater.inflate(R.layout.fragment_fourth, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onCreateView");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onCreateView");
+    }
+}
